d9ec46e763f0dbe0008b45ec1859e6e806ec7eb9
Igor14545216 <igor_ea@yahoo.com.br>
2021-01-20 14:25:21 -0300

    Add insert

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,9 +1,13 @@
+const Atendimento = require('../models/atendimentos')
+
 module.exports = app => {
     app.get('/atendimentos', (req, res) => res.send('você está na rota de atendimento testando GET'));
 
     app.post('/atendimentos', (req, res) => {
-        //teste enviado do postman
-        console.log(req.body)
+        const atendimento  = req.body;
+
+        Atendimento.adiciona(atendimento);
+
         res.send('você está na rota de atendimento testando POST')
     });
 }
