0d1a111777cecccec01979c44b577d7f8481ca02
Igor14545216 <igor_ea@yahoo.com.br>
2021-01-20 14:01:43 -0300

    scritp de criacao da query para tabela do banco

--- infraestrutura/tabelas.js
+++ infraestrutura/tabelas.js
@@ -1,6 +1,21 @@
 class Tabelas {
-    init(conexao){
+    init(conexao) {
+        this.conexao = conexao;
         console.log('Tabelas foram chamadas');
+
+        this.criarAtendimentos();
+    }
+
+    //script para criação de tabela dentro do banco
+    criarAtendimentos() {
+        const sql = 'CREATE TABLE Atendimentos (id int NOT NULL AUTO_INCREMENT, cliente varchar(50) NOT NULL, pet varchar(20), servico varchar(20) NOT NULL, status varchar(20) NOT NULL, observacoes text, PRIMARY KEY(id))'
+
+        this.conexao.query(sql, (erro) => {
+            if (erro)
+                console.log(erro);
+            else
+                console.log('Tabela atendimentos criada com sucesso');
+        })
     }
 }
 
