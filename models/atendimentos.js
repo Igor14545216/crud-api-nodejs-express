d3a4d53cf21adefa2fbd7662eab4aa5236dbfa9c
Igor14545216 <igor_ea@yahoo.com.br>
2021-01-20 15:04:31 -0300

    Manipulando datas

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,11 +1,17 @@
+const moment = require('moment')
 const conexao = require('../infraestrutura/conexao')
 
 class Atendimento {
-    // o que chegar de parametro vou insrir dentro da tabela Atendimentos do meu banco
+
+    // o que chegar de parametro do postman vou insrir dentro da tabela Atendimentos do meu banco
     adiciona(atendimento) {
+
+        const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS')
+        const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS') //data que eu mando do postman
+        const atendimentoDatado = { ...atendimento, dataCriacao, data } //pego tudo que estiver vindo do meu post e acrescendo o dataCriacao
         const sql = 'INSERT INTO Atendimentos SET ?'
 
-        conexao.query(sql, atendimento, (erro, resultados) => {
+        conexao.query(sql, atendimentoDatado, (erro, resultados) => {
             if (erro)
                 console.log(erro);
             else
