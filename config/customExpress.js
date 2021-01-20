69f06df5aaf6b6f0a0df83349cb4c9d0608f7d54
Igor14545216 <igor_ea@yahoo.com.br>
2021-01-20 11:01:32 -0300

    Add configuracoes de tratamento do post

--- config/customExpress.js
+++ config/customExpress.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const consign = require('consign')
+const bodyParser = require('body-parser')
 
 module.exports = () => {
     const app = express()
+    
+    //para a conversão do body (aplicação pronta para receber dados urlencoded)
+    app.use(bodyParser.urlencoded({ extended: true })) 
+
+    //para a conversão do json (aplicação pronta para receber dados como json)
+    app.use(bodyParser.json())                         
 
     consign()
         .include('controllers')
--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,4 +1,10 @@
-module.exports = app  => {
-    app.get('/atendimentos', (req, res) => res.send('você está na rota de atendimento e testando nodemonrs'))
+module.exports = app => {
+    app.get('/atendimentos', (req, res) => res.send('você está na rota de atendimento testando GET'));
+
+    app.post('/atendimentos', (req, res) => {
+        //teste enviado do postman
+        console.log(req.body)
+        res.send('você está na rota de atendimento testando POST')
+    });
 }
 
